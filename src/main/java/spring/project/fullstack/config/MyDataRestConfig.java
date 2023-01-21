33edcbcf2cd721e59b009cee239c2dc4b1d5984c
mahrez1 <59502081+mahrez1@users.noreply.github.com>
2023-01-21 14:23:30 +0100

    expose IDS

--- src/main/java/spring/project/fullstack/config/MyDataRestConfig.java
+++ src/main/java/spring/project/fullstack/config/MyDataRestConfig.java
@@ -1,17 +1,31 @@
-package spring.project.fullstack.config;
+ package spring.project.fullstack.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
 import spring.project.fullstack.entity.Product;
 import spring.project.fullstack.entity.ProductCategory;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
+	private EntityManager entityManager ;
+	
+	@Autowired
+	public MyDataRestConfig(EntityManager theEntityManager )
+	{
+		entityManager = theEntityManager ;
+	}
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
 		// disable http for product and productCategory
@@ -26,6 +40,26 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 			.forDomainType(ProductCategory.class)
 			.withItemExposure((metdata, httpMethods)-> httpMethods.disable(theUnsupportedActions))
 			.withCollectionExposure((metdata, httpMethods)-> httpMethods.disable(theUnsupportedActions)) ;
+			
+			exposeIds(config) ;
+	
 	}
 
+	public void exposeIds(RepositoryRestConfiguration config)
+	{
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities() ;
+		List<Class> entityClasses = new ArrayList<>() ;
+		for (EntityType tempEntityType : entities)
+		{
+			entityClasses.add(tempEntityType.getJavaType()) ;
+		}
+		
+		Class[] domainTypes = 	entityClasses.toArray(new Class[0]) ;
+		config.exposeIdsFor(domainTypes) ;
+		
+	}
+	
+	
+	
+	
 }
